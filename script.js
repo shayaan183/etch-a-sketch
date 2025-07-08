c1abf4c0a16564bc860f00423588ab1ea557af30
Shayaan <shayaan.musthafa2005@gmail.com>
2025-07-07 22:45:27 -0600

    Added functionality to create a 16x16 grid

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector(".container");
+
+function createGrid(size) {
+    container.innerHTML = '';
+    const totalSquares = size * size;
+    const squareSize = container.clientWidth / size;
+
+    for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        container.appendChild(square);
+    }
+}
+
+createGrid(16);
