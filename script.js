2edc33460495d89a409fe924587d4a1d081c771b
Shayaan <shayaan.musthafa2005@gmail.com>
2025-08-23 07:40:05 -0600

    Created a helper function to set the paint mode

--- script.js
+++ script.js
@@ -42,32 +42,35 @@ function createGrid(size) {
         square.style.opacity = 0;
 
         square.addEventListener('mouseenter', () => {
-            if (currentMode == "color") {
-                square.style.opacity = 1;
-                square.style.backgroundColor = currentColor;
-            } else if (currentMode == "rainbow") {
-                square.style.opacity = 1;
-                let r = Math.floor(Math.random() * 256);
-                let g = Math.floor(Math.random() * 256);
-                let b = Math.floor(Math.random() * 256);
-                square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-            } else if (currentMode == "shading") {
-                let currentOpacity = parseFloat(square.style.opacity);
-                if (currentOpacity < 1) {
-                    currentOpacity += 0.1;
-                    square.style.opacity = currentOpacity;
-                    square.style.backgroundColor = "#000000";
-                }
-            } else if (currentMode == "eraser") {{
-                square.style.opacity = 0;
-                square.style.backgroundColor = "#ffffff";
-            }}
+            setPaintMode(square);
         });
 
         container.appendChild(square);
     }
 }
 
+function setPaintMode(square) {
+    if (currentMode == "color") {
+        square.style.opacity = 1;
+        square.style.backgroundColor = currentColor;
+    } else if (currentMode == "rainbow") {
+        square.style.opacity = 1;
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    } else if (currentMode == "shading") {
+        let currentOpacity = parseFloat(square.style.opacity);
+        if (currentOpacity < 1) {
+            currentOpacity += 0.1;
+            square.style.opacity = currentOpacity;
+            square.style.backgroundColor = "#000000";
+        }
+    } else if (currentMode == "eraser") {{
+        square.style.opacity = 0;
+        square.style.backgroundColor = "#ffffff";
+    }}
+}
 
 function resizeGrid(e) {
     let newSize = e;
